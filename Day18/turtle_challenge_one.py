1d2cbb4798c096d88c1c25cf5a6d9c36c4c4ab28
haftamuGm <haftamugebremaryambsrat7@gmail.com>
2024-11-17 12:07:41 +0300

    Completed

---
+++ Day18/turtle_challenge_one.py
@@ -0,0 +1,10 @@
+from turtle import Turtle,Screen
+from random import Random
+mytimy=Turtle()
+screen=Screen()
+for _ in range(4):
+    mytimy.left(90)
+    mytimy.forward(100)
+screen.exitonclick()
+
+
