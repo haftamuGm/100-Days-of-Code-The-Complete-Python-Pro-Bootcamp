fd6bcf067af6a9e271459cad2f74326cfd39d7ce
haftamuGm <haftamugebremaryambsrat7@gmail.com>
2024-11-18 12:53:18 +0300

    Completed

--- Day19/The_Turtles_Race.py
+++ Day19/The_Turtles_Race.py
@@ -3,6 +3,12 @@ import random
 screen=Screen()
 screen.listen()
 screen.setup(width=500,height=400)
+is_choose=True
+while is_choose:
+    user_color_choice=screen.textinput(title='choose background color',prompt="choose your favorite color  from 'pink','black'")
+    if user_color_choice in ['black','pink']:
+        screen.bgcolor(user_color_choice)
+        is_choose=False
 user_bet=screen.textinput(title="Make a Bet",prompt='which one turtles will win a race Enter Color ').lower()
 color=["red",'orange','yellow','green','blue','purple']
 all_turtle=[]
