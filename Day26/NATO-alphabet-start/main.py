3dc0e326589916505c01c2786fccefa77665fa76
haftamuGm <haftamugebremaryambsrat7@gmail.com>
2024-11-24 09:41:54 +0300

    Completed

--- Day26/NATO-alphabet-start/main.py
+++ Day26/NATO-alphabet-start/main.py
@@ -1,8 +1,6 @@
 import pandas as p
 d=p.read_csv("nato_phonetic_alphabet.csv")
-nato_phonetic={}
-for index,row in d.iterrows():
-    nato_phonetic[row.letter]=row.code
+nato_phonetic={row.letter:row.code for index,row in d.iterrows()}
 user=input("Enter a Word :").upper()
 list_user_phonetic=[]
 for i in user:
