cf5bb48d70a0864856be27cca156a48254b7ece6
haftamuGm <haftamugebremaryambsrat7@gmail.com>
2024-11-27 11:17:49 +0300

    Update main.py

--- Day26/NATO-alphabet-start/main.py
+++ Day26/NATO-alphabet-start/main.py
@@ -1,6 +1,13 @@
 import pandas as p
 d=p.read_csv("nato_phonetic_alphabet.csv")
 nato_phonetic={row.letter:row.code for index,row in d.iterrows()}
-user=input("Enter a Word :").upper()
-list_user_phonetic=[nato_phonetic[i] for i in user]
-print(list_user_phonetic)
+def generic_phontic():
+    user = input("Enter a Word :").upper()
+    try:
+        list_user_phonetic=[nato_phonetic[i] for i in user]
+    except KeyError:
+        print("Sorry , Only letter in the  alphabet please.")
+        generic_phontic()
+    else:
+        print(list_user_phonetic)
+generic_phontic()
