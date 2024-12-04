64a10a453f0bd6fff50b0b9f6c9c34a781459198
haftamuGm <haftamugebremaryambsrat7@gmail.com>
2024-12-04 07:40:53 +0300

    Completed

--- Day34/quizzler-app-start/data.py
+++
@@ -1,5 +0,0 @@
-import requests
-result=requests.get("https://opentdb.com/api.php?amount=10&category=22&difficulty=medium&type=boolean")
-question_data=result.raise_for_status()
-question_data=result.json()["results"]
-
