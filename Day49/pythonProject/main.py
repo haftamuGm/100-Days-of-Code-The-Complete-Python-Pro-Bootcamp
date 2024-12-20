5f1b4d8ba2f05a3e44d29c4bb109f9ce130299f5
haftamuGm <haftamugebremaryambsrat7@gmail.com>
2024-12-20 20:09:08 +0300

    Completed

--- Day49/pythonProject/main.py
+++ Day49/pythonProject/main.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 chrome_option=webdriver.ChromeOptions()
-gmail="my_own_email"
+gmail="my_own_email  "
 password="my_own_password"
 chrome_option.add_experimental_option("detach",True)
 driver=webdriver.Chrome(chrome_option)
